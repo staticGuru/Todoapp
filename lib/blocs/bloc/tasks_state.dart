e4d354fe29410a5cc4c052fc5d2fd3e2b496751c
staticGuru <vigneshguru274@gmail.com>
2023-01-12 23:25:53 +0530

    add the flutter bloc

---
+++ lib/blocs/bloc/tasks_state.dart
@@ -0,0 +1,10 @@
+part of 'tasks_bloc.dart';
+
+abstract class TasksState extends Equatable {
+  const TasksState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class TasksInitial extends TasksState {}
