7630ff8088552806373e0fd2e878f568bf7cc542
staticGuru <vigneshguru274@gmail.com>
2023-01-12 23:33:04 +0530

    add the bloc events

--- lib/blocs/bloc/tasks_event.dart
+++ lib/blocs/bloc/tasks_event.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'tasks_bloc.dart';
 
 abstract class TasksEvent extends Equatable {
@@ -5,4 +6,28 @@ abstract class TasksEvent extends Equatable {
 
   @override
   List<Object> get props => [];
+}
+class AddTask extends TasksEvent {
+  final Task task;
+  const AddTask({
+    required this.task,
+  }); 
+    @override
+  List<Object> get props => [task];
+}
+class UpdateTask extends TasksEvent {
+  final Task task;
+  const UpdateTask({
+    required this.task,
+  }); 
+    @override
+  List<Object> get props => [task];
+}
+class DeleteTask extends TasksEvent {
+  final Task task;
+  const DeleteTask({
+    required this.task,
+  }); 
+    @override
+  List<Object> get props => [task];
 }
