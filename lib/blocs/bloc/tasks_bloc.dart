e750bb8bdfde0bfc9e7690c4f90702d38b6bf432
staticGuru <vigneshguru274@gmail.com>
2023-01-12 23:41:59 +0530

    add content in the test bloc

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -7,9 +7,18 @@ part 'tasks_event.dart';
 part 'tasks_state.dart';
 
 class TasksBloc extends Bloc<TasksEvent, TasksState> {
-  TasksBloc() : super(TasksInitial()) {
-    on<TasksEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  TasksBloc() : super(const TasksState()) {
+    on<AddTask>(_onAddTask);
+    on<UpdateTask>(_onUpdateTask);
+    on<DeleteTask>(_onDeleteTask);
   }
+  void _onAddTask(AddTask event, Emitter<TasksState> emit) {
+    final state = this.state;
+    emit(TasksState(allTasks: List.from(state.allTasks)..add(event.task)));
+  }
+
+  void _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {}
+
+  void _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
+
 }
