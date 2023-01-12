355580a3c24c505d0e3716116a3b4a57a7a5f709
staticGuru <vigneshguru274@gmail.com>
2023-01-12 23:53:47 +0530

    connect the bloc builder

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,40 +1,40 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/bloc_exports.dart';
 import '../models/task.dart';
 import '../widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
   TasksScreen({Key? key}) : super(key: key);
-  // ignore: prefer_final_fields
-  List<Task> _tasksList = [
-    Task(title: "task1"),
-    Task(title: "task2"),
-    Task(title: "task3")
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Task App'), actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.add),
-        )
-      ]),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Chip(label: Text('Tasks')),
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> _tasksList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(title: const Text('Task App'), actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+            )
+          ]),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Chip(label: Text('Tasks')),
+              ),
+              TasksList(tasksList: _tasksList)
+            ],
           ),
-          TasksList(tasksList: _tasksList)
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add Task',
-        child: const Icon(Icons.add),
-      ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
